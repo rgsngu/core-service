12e7686a89e8c9e6d7f92e9e188306c81a019e99
rgsngu <43198835+rgsngu@users.noreply.github.com>
2025-12-16 19:06:26 +0530

    chore: remove Sample

--- src/main/java/com/trading/platform/core/repository/SampleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.trading.platform.core.repository;
-
-import com.trading.platform.core.entity.Sample;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SampleRepository extends JpaRepository<Sample, Long> { }
--- src/main/java/com/trading/platform/core/service/SampleService.java
+++
@@ -1,5 +0,0 @@
-package com.trading.platform.core.service;
-
-public interface SampleService {
-    String createSample(String msg);
-}
